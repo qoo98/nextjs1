44506fe8443eee326a860976f188ac3be6c60471
qoo98 <chopper1224110@gmail.com>
2022-08-21 15:01:22 +0900

    initial

---
+++ pages/contact-page.js
@@ -0,0 +1,26 @@
+import Layout from "../components/Layout";
+import Image from "next/image";
+
+const Contact = () => {
+    return (
+    <Layout title="Contact"> 
+        <div className="bg-white text-center shadow-xl p-8 w-80 rounded">
+            <div className="mt-4">
+                <p className="font-bold">Contact info</p>
+            </div>
+            <div className="flex justify-center mt-4">
+                <Image className="rounded-full"
+                src="/avatar.jpg"
+                width={60}
+                height={60}
+                alt="avatar"/>
+            </div>
+        </div>
+      </Layout>
+     
+     );
+    
+  };
+  
+  export default Contact;
+  
